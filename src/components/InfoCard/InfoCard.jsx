8e495e342e9887d082749ad33bca5b719dba70eb
Durga Sumesh <durgasumesh17@gmail.com>
2025-05-19 18:46:41 +0530

    add link to register now button

--- src/components/InfoCard/InfoCard.jsx
+++ src/components/InfoCard/InfoCard.jsx
@@ -10,7 +10,7 @@ function InfoCard() {
           <h3>Mind Empowered Presents</h3>
           <h1>Starlet Hackathon</h1>
           <p> Empowering Women and Gender Minorities</p>
-          <button>Register Now</button>
+          <a href="https://docs.google.com/forms/d/e/1FAIpQLSeyP9JjvID93n8_zsTEk16sDpXvbi-4EieycQCi-hh-QGBqGA/viewform?fbclid=PAZXh0bgNhZW0CMTEAAadI4cMeIHnqgL9UbQOnCYaGk4OomIS1Yv9asHtH9GaR7Q_dS5SVU-neAuilYg_aem_7kXmfgbUjQylXHON8jc-uw " target="_blank" rel="noopener noreferrer"><button>Register Now</button></a>
         </div>
       </div>
     )
